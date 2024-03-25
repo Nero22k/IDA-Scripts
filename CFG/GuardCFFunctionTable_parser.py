8091fcaa76c1903c735f9c1b9a01c957f3393d91
Nero22k <64486541+Nero22k@users.noreply.github.com>
2024-03-25 11:39:01 +0000

    Update GuardCFFunctionTable_parser.py

--- CFG/GuardCFFunctionTable_parser.py
+++ CFG/GuardCFFunctionTable_parser.py
@@ -2,6 +2,16 @@ import idaapi
 import idc
 import idautils
 
+def decompile_function(func_ea):
+    try:
+        cfunc = idaapi.decompile(func_ea)
+        if cfunc:
+            return str(cfunc)
+        else:
+            return None
+    except idaapi.DecompilationFailure:
+        return None
+
 def get_func_args_count(func_ea):
     func_type = idaapi.tinfo_t()
     if not idaapi.get_tinfo(func_type, func_ea):
@@ -13,7 +23,24 @@ def get_func_args_count(func_ea):
 
     return func_data.size()
 
-def show_gfids_functions(args_count=None):
+def get_function_signature(func_ea):
+    func_type = idaapi.tinfo_t()
+    if not idaapi.get_tinfo(func_type, func_ea):
+        return None
+
+    func_name = idc.get_func_name(func_ea)
+    func_signature = idaapi.print_type(func_ea, idaapi.PRTYPE_1LINE)
+
+    return f"{func_signature}"
+
+def get_function_size(func_ea):
+    func = idaapi.get_func(func_ea)
+    if func is None:
+        return None
+
+    return func.size()
+
+def show_gfids_functions(args_count=None, decompile=False):
     guard_fids_seg = idaapi.get_segm_by_name("GFIDS")
     if not guard_fids_seg:
         print("GFIDS segment not found.")
@@ -27,22 +54,30 @@ def show_gfids_functions(args_count=None):
             func_name = idc.get_func_name(func_ea)
 
             if func_name:
+                if decompile:
+                    # Decompile the function if decompile flag is set (WARNING: This can take some time if you are parsing huge GFIDS table)
+                    decompiled_func = decompile_function(func_ea)
+                    if not decompiled_func:
+                        continue
+
                 args_count_found = get_func_args_count(func_ea)
                 if args_count is None or (args_count_found is not None and args_count_found == args_count):
-                    valid_functions.append([func_name, f"0x{func_rva:08X}", str(args_count_found)])
+                    func_signature = get_function_signature(func_ea)
+                    func_size = get_function_size(func_ea)
+                    valid_functions.append([func_name, f"0x{func_rva:08X}", str(args_count_found), func_signature, str(func_size)])
 
     if valid_functions:
         print("Guard FIDS Functions:")
-        print("{:<40} {:<10} {:<10}".format("Function Name", "RVA", "Arguments"))
-        print("-" * 60)
+        print("{:<40} {:<10} {:<10} {:<60} {:<20}".format("Function Name", "RVA", "Arguments", "Signature", "Size"))
+        print("-" * 130)
         for func in valid_functions:
-            print("{:<40} {:<10} {:<10}".format(func[0], func[1], func[2]))
+            print("{:<40} {:<10} {:<10} {} {:<20}".format(func[0], func[1], func[2], func[3], func[4]))
     else:
         print("No valid functions found.")
 
     return len(valid_functions)
 
-all_functions_count = show_gfids_functions()
+all_functions_count = show_gfids_functions(decompile=True)
 print()
 specific_args_count = show_gfids_functions(args_count=2)
 print(f"\nTotal count of functions with 2 arguments: {specific_args_count}")
